31f76ad287ee3e9971c2ab52719ba867d3c7fbda
岩澤 忠恭 <t_iwasawa@yumemi.co.jp>
2018-11-30 10:34:48 +0900

    FirstCommit

---
+++ BabyDriver/BabyDriverTests/BabyDriverTests.swift
@@ -0,0 +1,34 @@
+//
+//  BabyDriverTests.swift
+//  BabyDriverTests
+//
+//  Created by 岩澤 忠恭 on 2018/11/30.
+//  Copyright © 2018年 岩澤 忠恭. All rights reserved.
+//
+
+import XCTest
+@testable import BabyDriver
+
+class BabyDriverTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
